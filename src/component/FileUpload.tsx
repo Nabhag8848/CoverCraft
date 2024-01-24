dac95319b8a392b807eefd217a584b6f8980281b
Nabhag8848 <nabhagmotivaras76@gmail.com>
2024-01-24 21:39:24 +0530

    feat(ui): select layers and images directly from figma playground

--- src/component/FileUpload.tsx
+++ src/component/FileUpload.tsx
@@ -4,17 +4,47 @@ import {
   Stack,
   Text,
 } from "@create-figma-plugin/ui";
-import { h } from "preact";
+import { Fragment, h } from "preact";
+import { useState } from "preact/hooks";
 function FileUpload() {
+  const [image, setImage] = useState("");
+  window.onmessage = (event) => {
+    const pluginEvent = event.data.pluginMessage;
+    const { type, node } = pluginEvent;
+    switch (type) {
+      case "no_node_intended": {
+        setImage("");
+        break;
+      }
+      case "encoded_node": {
+        setImage(node);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
   function handleSelectedFiles(files: Array<File>) {}
+
   return (
-    <FileUploadDropzone onSelectedFiles={handleSelectedFiles}>
-      <Stack space="large" style={{ marginTop: "50px", marginBottom: "50px" }}>
-        <Text align="center">
-          <Muted>Select an Cover Image</Muted>
-        </Text>
-      </Stack>
-    </FileUploadDropzone>
+    <Fragment>
+      {image ? (
+        <img src={image} alt="Cover Image" width="318px" height="172px" />
+      ) : (
+        <FileUploadDropzone onSelectedFiles={handleSelectedFiles}>
+          <Stack
+            space="large"
+            style={{ marginTop: "50px", marginBottom: "50px" }}
+          >
+            <Text align="center">
+              <Muted>Select an Cover Image</Muted>
+            </Text>
+          </Stack>
+        </FileUploadDropzone>
+      )}
+    </Fragment>
   );
 }
 
